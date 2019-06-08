6b3e23d40fc94bfebadf0f53980a6fdba5b0db38
Agustin Calatayud <acalatayud@itba.edu.ar>
2019-06-08 16:31:42 -0300

    Solve overflow problem

--- src/matrix/matrix.c
+++ src/matrix/matrix.c
@@ -177,10 +177,11 @@ matrix_t * multiply(const matrix_t * m1, const matrix_t * m2, uint8_t mod) {
   size_t i, j, k;
   for (i = 0; i < m1->rows; i++) {
     for (j = 0; j < m2T->rows; j++) {
+      uint16_t value = 0;
       for (k = 0; k < m2T->columns; k++) {
-        result->data[i][j] += (m1->data[i][k] * m2T->data[j][k]);
+        value += (m1->data[i][k] * m2T->data[j][k]);
       }
-      result->data[i][j] %= mod;
+      result->data[i][j] = (uint8_t) (value % mod);
     }
   }
 
